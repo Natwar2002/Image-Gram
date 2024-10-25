f40efab9b419c4451f4102f744568ff99b8e0bf0
Natwar Patidar <natvarp6062@gmail.com>
2024-10-25 23:12:23 +0530

    Add auth

--- src/routers/v1/post.js
+++ src/routers/v1/post.js
@@ -3,13 +3,13 @@ import upload from "../../config/multerConfig.js";
 import { createPost, deletePost, getAllPosts, updatePost } from "../../controllers/postController.js";
 import { validate } from "../../validators/zodValidators.js";
 import { zodPostSchema } from "../../validators/zodPostSchema.js";
-import { isAuthenticated } from "../../middlewares/authMiddleware.js";
+import { isAdmin, isAuthenticated } from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/', isAuthenticated, upload.single('image'), validate(zodPostSchema), createPost);
 router.get('/', getAllPosts);
 router.delete('/:id', isAuthenticated, deletePost);
-router.put('/:id', upload.single('image'), updatePost);
+router.put('/:id', isAuthenticated, isAdmin, upload.single('image'), updatePost);
 
 export default router;
